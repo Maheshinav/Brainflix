5f50bcfc830a45bfb06ac14dc7769ffde91b76ee
ayodyapraneeth <ayodya.praneeth@gmail.com>
2023-06-24 16:28:52 -0700

    Make the structure of Header component

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,8 +1,39 @@
+/* eslint-disable no-unused-vars */
+import logo from "../../assets/images/BrainFlix-logo.svg";
+import "../../assets/images/upload.svg";
+import "../../assets/images/search.svg";
+import "./nav.css";
+
+
 
 const Nav = () => {
+
     return (
 
-      
+        <header >
+            <div className="header" >
+                <div className="header__container">
+                    <div className="header__logo-container">
+                        <img className="header__logo" src={logo} alt="logo -BrainFlix">
+                        </img>
+                    </div>
+                    <div className="header__search-wrap">
+                        <input
+                            className="header__search-input"
+                            type="search"
+                            placeholder="Search"
+                        />
+                        <div className="header__avatar"></div>
+                    </div>
+                    <button className="header__button">upload</button>
+
+                </div>
+            </div>
+
+        </header>
+
+
     )
 
 }
+export default Nav;
