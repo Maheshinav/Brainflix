2d0765366c57650d837827ede06642e527047bfc
ayodyapraneeth <ayodya.praneeth@gmail.com>
2023-06-26 07:23:51 -0700

    Fix my project

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 
 import Nav from "./components/nav/nav";
 
+
+// eslint-disable-next-line no-unused-vars
 function App() {
   return (
     <>
-      <Nav/>
+      <Nav />
     </>
 
   );
