a17153fb88e6ab27bc8f9fae5f39567f8fc3fc03
ayodyapraneeth <ayodya.praneeth@gmail.com>
2023-06-26 02:54:35 -0700

    finish the list component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
-import Main from "./components/main/Main";
+import Main from "./components/main/main";
 import videoData from "./data/video-details.json";
 import './data/video-details.json';
 import './data/videos.json';
-import Nav from "./components/nav/Nav";
+import Nav from "./components/nav/nav";
 
 function App() {
   return (
     <div>
       <Nav />
-      <Main videoData={videoData}/>
+      <Main videoData={videoData} />
+
     </div>
 
   );
