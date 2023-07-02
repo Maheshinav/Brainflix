941aad81e435579d955e38daff6f78ae99f459b0
ayodyapraneeth <ayodya.praneeth@gmail.com>
2023-07-01 22:50:54 -0700

    moving to the main branch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import './App.css';
-import Main from "./components/main/main";
-import videoData from "./data/video-details.json";
-import './data/video-details.json';
-import './data/videos.json';
-import Nav from "./components/nav/nav";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import "./data/video-details.json";
+import "./data/videos.json";
+import Nav from "./components/nav/Nav";
+import HomePage from "./pages/home-page/HomePage";
+import VideoDetails from "./pages/video -details/VideoDetails";
 
 function App() {
   return (
-    <div>
-      <Nav />
-      <Main videoData={videoData} />
-
-    </div>
-
+    <BrowserRouter>
+      <div>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/video/:id" element={<VideoDetails/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
